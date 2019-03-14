cd187baad54600d87aa2861ca6250d14f3699a80
Estevan Maito <ejmaito@gmail.com>
2019-03-14 11:39:08 -0300

    split apps into own folder

--- src/class-component/index.test.js
+++ src/class-component/index.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import App from "./App";
-import { Input, Button, TodoList } from "./App";
+import App from ".";
+import { Input, Button, TodoList } from ".";
 
 Enzyme.configure({ adapter: new Adapter() });
 const mount = Enzyme.mount;
